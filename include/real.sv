7cf59a4a0cd5e36c67d7e848f96d0d99846b2bef
Steven Herbst <sherbst@stanford.edu>
2019-05-24 19:39:44 -0700

    fixed bugs in range checking assertions

--- include/real.sv
+++ include/real.sv
@@ -94,7 +94,8 @@
 
     `define ASSERTION_REAL(in_name) \
         assertion_real #( \
-            `PASS_REAL(in, in_name) \
+            `PASS_REAL(in, in_name), \
+            .name(`"in_name`") \
         ) assertion_real_``in_name``_i ( \
             .in(in_name) \
         )
@@ -108,7 +109,7 @@
         localparam real `RANGE_PARAM_REAL(name) = range_expr; \
         localparam integer `WIDTH_PARAM_REAL(name) = width_expr; \
         localparam integer `EXPONENT_PARAM_REAL(name) = exponent_expr \
-        `ifdef DEBUG_REAL \
+        `ifdef RANGE_ASSERTIONS \
             ; `ASSERTION_REAL(name) \
         `endif
 
