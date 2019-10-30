79d333bf8e022d206b60b8d1d989619056c2417b
Steven Herbst <sherbst@stanford.edu>
2019-10-30 14:39:41 -0700

    added dff tests

---
+++ tests/test_dff.py
@@ -0,0 +1,40 @@
+from .common import *
+
+TOP = 'test_dff'
+PROJECT = 'test_dff'
+FILES = ['test_dff.sv']
+
+def pytest_generate_tests(metafunc):
+    pytest_sim_params(metafunc)
+    #metafunc.parametrize('defs', [None, ['SVREAL_DEBUG']])
+    metafunc.parametrize('defs', [['SVREAL_DEBUG']])
+
+def test_ops(simulator, defs):
+    # run sim
+    res=run_sim(*FILES, top=TOP, project=PROJECT, simulator=simulator, defs=defs)
+
+    # parse results
+    res = parse_stdout(res.stdout)
+
+    # check results
+    sec = res[1]
+    assert is_close(sec['d'], +2.34)
+    assert is_close(sec['q'], +0.00)
+    sec = res[2]
+    assert is_close(sec['d'], +2.34)
+    assert is_close(sec['q'], +0.00)
+    sec = res[3]
+    assert is_close(sec['d'], +2.34)
+    assert is_close(sec['q'], +2.34)
+    sec = res[4]
+    assert is_close(sec['d'], +3.45)
+    assert is_close(sec['q'], +2.34)
+    sec = res[5]
+    assert is_close(sec['d'], +3.45)
+    assert is_close(sec['q'], +3.45)
+    sec = res[6]
+    assert is_close(sec['d'], +4.56)
+    assert is_close(sec['q'], +3.45)
+    sec = res[7]
+    assert is_close(sec['d'], +4.56)
+    assert is_close(sec['q'], +3.45)
---
+++ tests/test_dff.sv
@@ -0,0 +1,82 @@
+`timescale 1ns / 1ps
+
+`include "svreal.sv"
+
+module test_dff;
+
+    // create signals
+    `MAKE_SVREAL(d, 16, -8);
+    `MAKE_SVREAL(q, 17, -9);
+    logic clk, rst, ce;
+    `SVREAL_DFF(d, q, rst, clk, ce);
+
+    task print_signals();
+        `SVREAL_PRINT(d);
+        `SVREAL_PRINT(q);
+        $display("clk=%0b", clk);
+        $display("rst=%0b", rst);
+        $display("ce=%0b", ce);
+    endtask
+
+    // create testbench
+    initial begin
+        // print header
+        $display("SVREAL TEST START");
+        #(1ns);
+
+        // read out the reset value
+        `SVREAL_SET(d, 2.34);
+        clk = 1'b0;
+        rst = 1'b1;
+        ce = 1'b1;
+        #(1ns);
+        clk = 1'b1;
+        #(1ns);
+        $display("SVREAL TEST SET 1");
+        print_signals();
+
+        // clock in the first value
+        `SVREAL_SET(d, 2.34);
+        clk = 1'b0;
+        rst = 1'b0;
+        ce = 1'b1;
+        #(1ns);
+        clk = 1'b1;
+        $display("SVREAL TEST SET 2");
+        print_signals();
+        #(1ns);
+        $display("SVREAL TEST SET 3");
+        print_signals();
+
+        // clock in the second value
+        `SVREAL_SET(d, 3.45);
+        clk = 1'b0;
+        rst = 1'b0;
+        ce = 1'b1;
+        #(1ns);
+        $display("SVREAL TEST SET 4");
+        print_signals();
+        clk = 1'b1;
+        #(1ns);
+        $display("SVREAL TEST SET 5");
+        print_signals();
+
+        // disable clock
+        `SVREAL_SET(d, 4.56);
+        clk = 1'b0;
+        rst = 1'b0;
+        ce = 1'b0;
+        #(1ns);
+        $display("SVREAL TEST SET 6");
+        print_signals();
+        clk = 1'b1;
+        #(1ns);
+        $display("SVREAL TEST SET 7");
+        print_signals();
+
+        // print footer
+        $display("SVREAL TEST END");
+        #(1ns);
+    end
+
+endmodule
