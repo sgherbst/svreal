d81f8cbfd23848cf394e5b620f3c6836eb60ccb5
Steven Herbst <sherbst@stanford.edu>
2020-03-13 20:24:36 -0700

    try to use the regular version of fault

--- regress.sh
+++ regress.sh
@@ -4,11 +4,8 @@ pip install -e .
 # install a specific version of pysmt to avoid cluttering the output with warnings
 pip install pysmt==0.8.1.dev93
 
-# install a specific version of fault
-pip install -e git://github.com/leonardt/fault.git@paths_w_spaces_2#egg=fault
-
 # install testing dependencies
-pip install pytest pytest-cov mantle
+pip install pytest pytest-cov mantle fault
 
 # run tests
 pytest --cov-report=xml --cov=svreal tests/ -v -r s
