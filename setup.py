1bdd4f678ee9528e4fb8466c1b5ef02f29672d58
Steven Herbst <sherbst@stanford.edu>
2020-08-14 16:35:57 -0700

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 name = 'svreal'
-version = '0.2.5.dev5'
+version = '0.2.5.dev6'
 
 DESCRIPTION = '''\
 Library for working with fixed-point numbers in SystemVerilog\
