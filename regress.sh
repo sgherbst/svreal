d835c845fa24ffe08caa1811dd2c70e4e122908f
Steven Herbst <sgherbst@users.noreply.github.com>
2020-10-28 11:44:55 -0700

    Pin magma ecosystem dependencies

--- regress.sh
+++ regress.sh
@@ -6,10 +6,10 @@ source install_hardfloat.sh
 pip install -e .
 
 # install a specific version of pysmt to avoid cluttering the output with warnings
-pip install pysmt==0.8.1.dev93
+pip install pysmt==0.9.0
 
 # install testing dependencies
-pip install pytest pytest-cov mantle fault
+pip install pytest pytest-cov fault==3.0.36 magma-lang==2.1.17 coreir==2.0.120 mantle==2.0.10 hwtypes==1.4.3 ast_tools==0.0.30 kratos==0.0.31.1
 
 # run tests
 pytest --cov-report=xml --cov=svreal tests/ -v -r s
